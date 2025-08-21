2f77eb2316ec71db8c34fe4ed845df1ff67ef7c4
ANANDU V K <96731751+ANANDUVK@users.noreply.github.com>
2025-08-21 10:56:47 +0530

    Update gsm3.c
    new gsm.c ,  need some modification including provide AT commands as #define macros

--- Safe_Within/Core/Src/gsm3.c
+++ Safe_Within/Core/Src/gsm3.c
@@ -6,49 +6,46 @@ extern UART_HandleTypeDef huart4;
 
 #define RX_BUFFER_SIZE 100
 #define GSM_MAX_RETRIES 3
+#define AT "AT"
+#define ATD "ATD+918129409935;"
+#define OK "OK"
 
-typedef enum {
-    GSM_STAGE_WAKE = 0,
-    GSM_STAGE_ECHO_OFF,
-    GSM_STAGE_READY,
-    GSM_STAGE_DIAL,
-    GSM_STAGE_DONE,
-    GSM_STAGE_ERROR
-} GSM_State_t;
 
 uint8_t rx_buffer[RX_BUFFER_SIZE];
 uint16_t rx_len = 0;
-//GSM_State_t gsm_state = GSM_STAGE_WAKE;
 
 int gsm_receive_response(uint32_t timeout_ms) {
 	 HAL_StatusTypeDef status;
+	 int response_count=0;
+	 while(response_count<3){
 	memset(rx_buffer, 0, RX_BUFFER_SIZE);
 
-    status = HAL_UART_Receive(&huart4, rx_buffer, RX_BUFFER_SIZE - 1, timeout_ms);
-//		 HAL_UART_Receive(&huart4, rx_buffer, RX_BUFFER_SIZE - 1, timeout_ms);
-		 rx_buffer[RX_BUFFER_SIZE - 1] = '\0';
+    status= HAL_UART_Receive(&huart4, rx_buffer, RX_BUFFER_SIZE - 1, timeout_ms);
 
-		    if (status == HAL_OK) {
-		        return 1;  // success
-		    } else {
-		        return 0;  // fail or timeout
-		    }
+
+    if (status == HAL_OK) {
+        return 1;  // Success
+    } else {
+        response_count++; // Increment and retry
+    }
+	 }
+	 return 0; //fail after three times
 }
 
 // Initialize GSM module
 int gsm_init(void) {
     // 1. Send first AT command
-    HAL_UART_Transmit(&huart4, (uint8_t*)"AT\r\n", strlen("AT\r\n"), 1000);
-    HAL_Delay(500);
+    HAL_UART_Transmit(&huart4, (uint8_t*)"AT\r\n", strlen("AT\r\n"), 10);
+    HAL_Delay(1000);
 
     // 2. Send second AT command and receive response, verify echo and OK
-    HAL_UART_Transmit(&huart4, (uint8_t*)"AT\r\n", strlen("AT\r\n"), 1000);
+    HAL_UART_Transmit(&huart4, (uint8_t*)"AT\r\n", strlen("AT\r\n"), 10);
     if (!gsm_receive_response(1000)) {
             return 0;  // receive failed
         }
-    if (!(strstr((char*)rx_buffer, "AT\r") != NULL &&
-        strstr((char*)rx_buffer, "OK\r\n") != NULL)) {
-    	HAL_UART_Transmit(&huart4, (uint8_t*)"AT\r\n", strlen("AT\r\n"), 1000);
+    if (!(strstr((char*)rx_buffer,AT) != NULL &&
+        strstr((char*)rx_buffer,OK) != NULL)) {
+    	HAL_UART_Transmit(&huart4, (uint8_t*)"AT\r\n", strlen("AT\r\n"), 10);
     	return 0;
     }
 
@@ -61,21 +58,24 @@ int gsm_call(void) {
 
     int retries = 0;
     while (retries < GSM_MAX_RETRIES) {
-        HAL_UART_Transmit(&huart4, (uint8_t*)cmd, strlen(cmd),1000);
-        if (gsm_receive_response(5000)) {
-            if (strstr((char*)rx_buffer, "ATD+918129409935;\r") != NULL &&
-                strstr((char*)rx_buffer, "OK/r/n") != NULL) {
+        HAL_UART_Transmit(&huart4, (uint8_t*)cmd, strlen(cmd),10);
+        if (gsm_receive_response(1000)) {
+            if (strstr((char*)rx_buffer,ATD) != NULL &&
+                strstr((char*)rx_buffer,OK) != NULL) {
                 return 1;
             }
 
         }
         retries++;
-        HAL_Delay(500);
+        HAL_Delay(5000);
         }
     return 0;
 
 }
 
+int gsm_sms(){
+return 0;
+}
 
 
 
